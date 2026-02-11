99dccf32158c517b722c7cf9f907c8c81b4178f0
Danylo <danil.bogomazov1356@gmail.com>
2026-02-11 20:35:20 +0200

    Create copy_file func

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if len(command.split()) != 3:
+        return
+    cp, file_name1, file_name2 = command.split()
+    if (file_name1 == file_name2
+            or ".txt" not in file_name1
+            or ".txt" not in file_name2
+            or cp != "cp"):
+        return
+    try:
+        with open(file_name1, "r") as file, open(file_name2, "w") as copy:
+            for line in file:
+                copy.write(line)
+    except FileNotFoundError:
+        return
